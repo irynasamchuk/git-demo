084fff1032f2bef802e77d5b5255957e6c7f165a
Iryna Samchuk <samchukicat@gmail.com>
2019-10-10 16:05:44 +0300

    add ChromeOptions to the Driver Manager

--- src/main/java/DriverManager.java
+++ src/main/java/DriverManager.java
@@ -2,6 +2,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     protected static WebDriver driver;
@@ -9,8 +10,10 @@ public class DriverManager {
     @BeforeClass
     public static void openDriver(){
         if (driver == null){
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setBinary("C:\\Users\\Iryna_Samchuk\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
             System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(chromeOptions);
         }
     }
 
